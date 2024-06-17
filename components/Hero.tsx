09d7cbfcb14a5e70cb61dce3b4c7dc98576445d2
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-06-18 01:40:06 +0700

    feat: about and recent projects

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,12 +27,12 @@ const Hero = () => {
       {/* Text */}
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col justify-center items-center">
-          {/* <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
-          </h2> */}
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Ideas. Made Effortless.
+          </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Bridging the gap between vision and flawless interaction."
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi! I&apos;m Ceavin Rufus, a full-stack developer from Indonesia.
