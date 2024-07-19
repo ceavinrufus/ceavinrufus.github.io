6eb175553004c01cd48056488ec9f80e6c07acad
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-07-19 14:44:44 +0700

    hero on phone size

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -9,12 +9,18 @@ const Hero2 = ({
   scrollContainer: React.RefObject<HTMLDivElement>;
 }) => {
   const starsRef = useRef<any>(null);
+  const starsRef2 = useRef<any>(null);
 
   const handleScroll = () => {
     if (starsRef.current) {
       const scrollPosition = window.scrollY;
       const scale = 1.5 - scrollPosition / 3000; // Adjust the divisor to control the zoom speed
       starsRef.current.style.transform = `scale(${scale})`;
+    }
+    if (starsRef2.current) {
+      const scrollPosition = window.scrollY;
+      const scale = 1.1 - scrollPosition / 3000; // Adjust the divisor to control the zoom speed
+      starsRef2.current.style.transform = `scale(${scale})`;
     }
   };
 
@@ -27,7 +33,7 @@ const Hero2 = ({
 
   return (
     <section className="parallax">
-      <div className="parallax__content absolute top-[5%] sm:top-[10%] lg:top-[16%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row items-start z-10">
+      <div className="parallax__content absolute top-[4%] sm:top-[10%] lg:top-[16%] w-full md:mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row items-start z-10">
         <div className="flex-1 lg:mb-0">
           <h1 className="font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] leading-[110px]">
             Ceavin Rufus
@@ -36,12 +42,12 @@ const Hero2 = ({
         </div>
       </div>
 
-      {/* <img
-        ref={starsRef}
-        className="parallax__stars"
+      <img
+        ref={starsRef2}
+        className="parallax__stars lg:hidden scale-110"
         src="/assets/parallax/1Stars.svg"
         alt=""
-      /> */}
+      />
       <img
         className="parallax__planets"
         src="/assets/parallax/2Planets.svg"
@@ -65,7 +71,7 @@ const Hero2 = ({
       <img className="parallax__sun" src="/assets/parallax/6Sun.svg" alt="" />
       <div
         ref={starsRef}
-        className="parallax__monster scale-150 fixed top-0 left-0"
+        className="parallax__monster w-screen h-screen hidden lg:block scale-150 fixed top-0 left-0"
       >
         <Monster />
       </div>
