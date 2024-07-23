d04fdd6ee7e10efe1c59f36eb55113f00335d8f4
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-07-23 08:50:08 +0700

    krypt

--- data/techStack.ts
+++ data/techStack.ts
@@ -86,6 +86,15 @@ export const technologies = [
     name: "Supabase",
     icon: "/supabase.svg",
   },
+  // Web3
+  {
+    name: "Solidity",
+    icon: "/solidity.svg",
+  },
+  {
+    name: "Hardhat",
+    icon: "/hardhat.svg",
+  },
   // CMS
   {
     name: "DatoCMS",
