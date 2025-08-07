5ec49c92c2e6144ea0d182b7e5431af5cb7a3e0a
ceavinrufus <18221162@std.stei.itb.ac.id>
2025-08-07 15:30:56 +0700

    change telkom experience detail

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -158,7 +158,7 @@ export function Experiences() {
           <ExperienceTitle
             role="Software Engineer"
             company="Telkom Indonesia"
-            type="Internship"
+            type="Contract"
           />
           <div className="mb-8">
             <ExperienceListContent>
@@ -183,15 +183,6 @@ export function Experiences() {
               ✅ Orchestrated GitLab CI/CD pipelines, resulting in a 30%
               reduction in build times.
             </ExperienceListContent>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="/assets/projectImages/siaga-servo.png"
-              alt="Siaga servo"
-              width={500}
-              height={300}
-              className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
           </div>
         </div>
       ),
